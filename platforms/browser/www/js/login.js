07cd02bdd940e1b1ba0f16d64d9459f502e44cfc
Jalfor <jalerous@gmail.com>
2016-12-09 21:40:12 +1100

    Fixed login by remaking in Cordova

---
+++ platforms/browser/www/js/login.js
@@ -0,0 +1,72 @@
+function loginInit() {
+    currentPage = "LOGIN";
+    
+    $("#slideshow > div:gt(0)").hide();
+
+    setInterval(function() { 
+        $('#slideshow > div:first')
+            .fadeOut(2000)
+            .next()
+            .fadeIn(500)
+            .end()
+            .appendTo('#slideshow');
+    }, 6000);
+    
+    function verifyAccessToken() {
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            data: {
+                at: accessToken
+            },
+            url: SERVER_ADDRESS + '/login.php',
+            success: function(data) {
+                if (data[0] === "2") {    //Username authentication failure
+                    $("#infoText").html("Authentication Failure");
+                }
+                else if (data[0] === "1a") {   //Success (User already exists)
+                    pageTransition("match.html", matchInit);
+                }
+                else if (data[0] === "1b") {   //Success (User doesn't already exist)
+                    pageTransition("signup/page1.html", signupPage1);
+                }
+            },
+        }).fail(function(dunnoWhatThisArgumentDoes, textStatus) {
+            console.log(textStatus);
+        });
+    }
+    
+    document.getElementById("mainloader").style.visibility = 'hidden';
+    
+    //Login on the login page clicked
+    $("#login").click(function() {
+        function waitForFacebookToDeignToJoinUs() {
+            if (typeof(facebookConnectPlugin) == "undefined") {
+                setTimeout(function() {
+                    waitForFacebookToDeignToJoinUs();
+                }, 250);
+            }
+
+            else {
+                if (window.cordova.platformId == "browser") {
+                    facebookConnectPlugin.browserInit("303510073316531");
+                }
+
+                var fbLoginSuccess = function (userData) {
+                    accessToken = userData.authResponse.accessToken;
+                    console.log(userData.authResponse.accessToken);
+                    verifyAccessToken();
+                }
+
+                facebookConnectPlugin.login(["public_profile"],
+                    fbLoginSuccess,
+                    function (error) {
+                        console.log(error);
+                    }
+                );
+            }
+        }
+    
+        waitForFacebookToDeignToJoinUs();
+    });
+}
--- www/js/main.js
+++ www/js/main.js
@@ -8,7 +8,6 @@ var currentPage;
 var accessToken;
 
 $(document).ready(function() {
-    chatContactSelected = 'ass';
     pageTransition("splash.html", function() {});
     
     /*var fbLoginSuccess = function (userData) {
@@ -17,38 +16,44 @@ $(document).ready(function() {
     
     accessToken = window.localStorage.getItem("facebook_accessToken");
     
-    if (device.platform != "browser") {
+    //if (device.platform != "browser") {
         initApp();  //This happens in statusChangeCallback for browser
-    }
+    //}
 });
 
 function initApp() {
-    $.ajax({
-        type: "POST",
-        dataType: "json",
-        data: {
-            at: accessToken
-        },
-        url: SERVER_ADDRESS + '/login.php',
-        success: function(data) {
-            console.log(data[0]);
-            if (data[0] === "2") {    //Username authentication failure
-                pageTransition("login.html", loginInit);
-            }
-            else if (data[0] === "1a") {   //Success (User already exists)
-                pageTransition("match.html", matchInit);
-            }
-            else if (data[0] === "1b") {   //Success (User doesn't already exist)
-                pageTransition("signup/page1.html", signupPage1);
-            }
-            else {
-                pageTransition("login.html", loginInit);
-            }
-        },
-    }).fail(function(dunnoWhatThisArgumentDoes, textStatus) {
-        console.log("INIT FAILURE: " + textStatus);
+    if (accessToken == undefined) {
         pageTransition("login.html", loginInit);
-    });
+    }
+    
+    else {
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            data: {
+                at: accessToken
+            },
+            url: SERVER_ADDRESS + '/login.php',
+            success: function(data) {
+
+                if (data[0] === "2") {    //Username authentication failure
+                    pageTransition("login.html", loginInit);
+                }
+                else if (data[0] === "1a") {   //Success (User already exists)
+                    pageTransition("match.html", matchInit);
+                }
+                else if (data[0] === "1b") {   //Success (User doesn't already exist)
+                    pageTransition("signup/page1.html", signupPage1);
+                }
+                else {
+                    pageTransition("login.html", loginInit);
+                }
+            },
+        }).fail(function(dunnoWhatThisArgumentDoes, textStatus) {
+            console.log("INIT FAILURE: " + textStatus);
+            pageTransition("login.html", loginInit);
+        });
+    }
 }
 
 function pageTransition(pageURL, initFunction) {
@@ -104,78 +109,76 @@ function pageTransition(pageURL, initFunction) {
     }
 }
 
-    // This is called with the results from from FB.getLoginStatus().
-    function statusChangeCallback(response) {
+// This is called with the results from from FB.getLoginStatus().
+function statusChangeCallback(response) {
 
-        fbResponse = response;
-        // The response object is returned with a status field that lets the
-        // app know the current login status of the person.
-        // Full docs on the response object can be found in the documentation
-        // for FB.getLoginStatus().
-        if (response.status === 'connected') {
-          // Logged into your app and Facebook.
-        } else if (response.status === 'not_authorized') {
-          // The person is logged into Facebook, but not your app.
-          document.getElementById('status').innerHTML = 'Please log ' +
-            'into this app.';
-        } else {
-          // The person is not logged into Facebook, so we're not sure if
-          // they are logged into this app or not.
-          document.getElementById('status').innerHTML = 'Please log ' +
-            'into Facebook.';
-        }
-        
-        if (device.platform == "browser") {
-            accessToken = fbResponse.authResponse.accessToken;
-        }
+    fbResponse = response;
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    if (response.status === 'connected') {
+      // Logged into your app and Facebook.
+    } else if (response.status === 'not_authorized') {
+      // The person is logged into Facebook, but not your app.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into this app.';
+    } else {
+      // The person is not logged into Facebook, so we're not sure if
+      // they are logged into this app or not.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into Facebook.';
+    }
 
-        initApp();
-  }
+    if (device.platform == "browser") {
+        accessToken = fbResponse.authResponse.accessToken;
+    }
+    
+    initApp();
+}
 
-  // This function is called when someone finishes with the Login
-  // Button.  See the onlogin handler attached to it in the sample
-  // code below.
-  function checkLoginState() {
+// This function is called when someone finishes with the Login
+// Button.  See the onlogin handler attached to it in the sample
+// code below.
+function checkLoginState() {
     FB.getLoginStatus(function(response) {
-      statusChangeCallback(response);
+        statusChangeCallback(response);
     });
-  }
+}
 
-  window.fbAsyncInit = function() {
-  FB.init({
-    appId      : '303510073316531',
-    cookie     : true,  // enable cookies to allow the server to access 
-                        // the session
-    xfbml      : true,  // parse social plugins on this page
-    version    : 'v2.5' // use graph api version 2.5
-  });
+window.fbAsyncInit = function() {
+    FB.init({
+        appId      : '303510073316531',
+        cookie     : true,  // enable cookies to allow the server to access 
+                            // the session
+        xfbml      : true,  // parse social plugins on this page
+        version    : 'v2.5' // use graph api version 2.5
+    });
 
-  // Now that we've initialized the JavaScript SDK, we call 
-  // FB.getLoginStatus().  This function gets the state of the
-  // person visiting this page and can return one of three states to
-  // the callback you provide.  They can be:
-  //
-  // 1. Logged into your app ('connected')
-  // 2. Logged into Facebook, but not your app ('not_authorized')
-  // 3. Not logged into Facebook and can't tell if they are logged into
-  //    your app or not.
-  //
-  // These three cases are handled in the callback function.
+    // Now that we've initialized the JavaScript SDK, we call 
+    // FB.getLoginStatus().  This function gets the state of the
+    // person visiting this page and can return one of three states to
+    // the callback you provide.  They can be:
+    //
+    // 1. Logged into your app ('connected')
+    // 2. Logged into Facebook, but not your app ('not_authorized')
+    // 3. Not logged into Facebook and can't tell if they are logged into
+    //    your app or not.
+    //
+    // These three cases are handled in the callback function.
 
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-  });
+    FB.getLoginStatus(function(response) {
+        statusChangeCallback(response);
+    });
 
-  };
+};
 
-  // Load the SDK asynchronously
-  (function(d, s, id) {
+// Load the SDK asynchronously
+(function(d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
+    if (d.getElementById(id)) 
+        return;
     js = d.createElement(s); js.id = id;
     js.src = "//connect.facebook.net/en_US/sdk.js";
     fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
-
-  // Here we run a very simple test of the Graph API after login is
-  // successful.  See statusChangeCallback() for when this call is made.
+}(document, 'script', 'facebook-jssdk'));
