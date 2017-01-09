fec362dd76f131e358a9277cf54ba94b7d8a2a5c
Jalfor <jalerous@gmail.com>
2017-01-09 13:03:01 +1100

    Conversations are functional

--- platforms/browser/www/js/chat.js
+++ platforms/browser/www/js/chat.js
@@ -1,12 +1,59 @@
 function chatInit() {
+    currentPage = "CHAT";
+    
+    var interval = null;
+    
+    function updateMessages() {
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            data: {
+                ato: accessToken,
+                contact_id: contactID,
+                req_type: "check_cache"
+            },
+            url: SERVER_ADDRESS + "/chat.php",
+            success: function(data) {
+
+                //Data is terminated by empty string
+                //and is in the form id, message, id,
+                //message etc.
+                for (var i = 0; data[i] != ""; i += 2) {
+                    var id = data[i];
+                    var message = data[i + 1];
+
+                    if (id == userID) {
+                         $("#message-area").append('\
+                            <div class="talk-bubble tri-right round btm-right-in user">\
+                                <div class="talktext">\
+                                    <p>' + message + '</p>\
+                                </div>\
+                            </div>');
+                    }
+
+                    else if (id == contactID) {
+                        $("#message-area").append('\
+                            <div class="talk-bubble tri-right round btm-left-in match">\
+                                <div class="talktext">\
+                                    <p>' + message + '</p>\
+                                </div>\
+                            </div>');
+                    }
+                }
+            },
+        }).fail(function(dunnoWhatThisArgumentDoes, textStatus) {
+            console.log(textStatus);
+        });
+    }
+    
     //The back button on the contacts page is clicked
     $("#back-button").click(function() {
+        clearInterval(interval);    //Halt message updates
         pageTransition("messages.html", messagesInit);
     });
     
     
     //Fill the messages list
-    //This request is for getting active conversations
     $.ajax({
         type: "POST",
         dataType: "json",
@@ -17,37 +64,80 @@ function chatInit() {
         },
         url: SERVER_ADDRESS + "/chat.php",
         success: function(data) {
-            console.log(data);
-            /*
-            <div class="talk-bubble tri-right round btm-right-in user">
-              <div class="talktext">
-                <p>Please stop messaging me.</p>
-              </div>
-            </div>*/
+            
+            //Data is terminated by empty string
+            //and is in the form id, message, id,
+            //message etc.
+            for (var i = 0; data[i] != ""; i += 2) {
+                var id = data[i];
+                var message = data[i + 1];
+                
+                if (id == userID) {
+                     $("#message-area").append('\
+                        <div class="talk-bubble tri-right round btm-right-in user">\
+                            <div class="talktext">\
+                                <p>' + message + '</p>\
+                            </div>\
+                        </div>');
+                }
+                
+                else if (id == contactID) {
+                    $("#message-area").append('\
+                        <div class="talk-bubble tri-right round btm-left-in match">\
+                            <div class="talktext">\
+                                <p>' + message + '</p>\
+                            </div>\
+                        </div>');
+                }
+            }
+            
+            setInterval(updateMessages, 3000);
         },
     }).fail(function(dunnoWhatThisArgumentDoes, textStatus) {
-            console.log(textStatus);
+        console.log(textStatus);
     });
     
-    $("#listBackground").click(function() {
-        chatContactSelected = $(this).text();
-        pageTransition("messages.html", messagesInit);
+    $("#chat-submit").click(function() {
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            data: {
+                ato: accessToken,
+                contact_id: contactID,
+                req_type: "send",
+                message: $("#chat-textbox").val()
+            },
+            url: SERVER_ADDRESS + "/chat.php",
+            success: function(data) {
+                
+            },
+        }).fail(function(dunnoWhatThisArgumentDoes, textStatus) {
+                console.log(textStatus);
+        });
+
+        $("#chat-textbox").val("");
     });
-    /*
-    $.ajax({
-        type: "POST",
-        dataType: "json",
-        data: {
-            ato: accessToken,
-            contact_id: contactID,
-            req_type: "send",
-            message: "nudes plz"
-        },
-        url: SERVER_ADDRESS + "/chat.php",
-        success: function(data) {
-            console.log(data);
-        },
-    }).fail(function(dunnoWhatThisArgumentDoes, textStatus) {
-            console.log(textStatus);
-    });*/
+    
+    $("#chat-textbox").on("keyup", function (e) {
+        if(e.which === 13){    //Enter
+            $.ajax({
+                type: "POST",
+                dataType: "json",
+                data: {
+                    ato: accessToken,
+                    contact_id: contactID,
+                    req_type: "send",
+                    message: $("#chat-textbox").val()
+                },
+                url: SERVER_ADDRESS + "/chat.php",
+                success: function(data) {
+                    
+                },
+            }).fail(function(dunnoWhatThisArgumentDoes, textStatus) {
+                    console.log(textStatus);
+            });
+
+            $("#chat-textbox").val("");
+        }
+    });
 }
