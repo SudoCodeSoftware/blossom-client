b334995dd7e0c9a7f1e8d506a29d4bce2812df75
Jonathan Moallem <jdmoallem@gmail.com>
2017-01-30 22:44:39 +1100

    Linked settings.html

--- www/js/match.js
+++ www/js/match.js
@@ -1,17 +1,17 @@
 function matchInit() {
     globals.currentPage = "MATCH";
     globals.match = {};
-    
+
     $("#user-portrait").css("background-image", "url('" + globals.profilePicURL + "')");
-    
+
     var matches = [0];  //the 0 is shifted off the end on the first card swap
     var matchCacheSize = 3; //how many matches before more are fetched
-    
+
     //Chat button at top right
     $("#messages-side").click(function() {
         pageTransition("messages.html", messagesInit);
     });
-    
+
     //Tick button (bottom right)
     $("#tick").click(function() {
         console.log(matches[0]);
@@ -28,13 +28,13 @@ function matchInit() {
                 console.log(data);
                 if (data[0] != '0') {   //The other person matched
                     $("#match-ticked-popup").load("tickPopup.html", function() {
-                        
+
                         $("#close-match-popup").click(function() {
                             $("#match-ticked-popup").html("");
                         });
-                        
+
                         $("#match-popup-match-pic").css("background-image", "url('" + data[1].cover + "')");
-                        
+
                         $("#new_match_poppup_user_name").html(data[1].name);
                     });
                 }
@@ -42,22 +42,26 @@ function matchInit() {
         }).fail(function(dunnoWhatThisArgumentDoes, textStatus) {
             console.log(textStatus);
         });
-        
+
         nextCard();
     });
-    
+
     //Cross button (bottom left)
     $("#cross").click(function() {
         nextCard();
     });
-    
+
     $("#user-portrait").click(function() {
         globals.match.userSelected = globals.userID;
         pageTransition("profile.html", profileInit);
     });
-    
+
+    $("#settings-side").click(function() {
+        pageTransition("settings.html", settingsInit);
+    });
+
     initMatches();
-    
+
     //Identical to updateMatches, except that this
     //automatically loads the first card
     function initMatches() {
@@ -78,7 +82,7 @@ function matchInit() {
             console.log(textStatus);
         });
     }
-    
+
     function updateMatches() {
         $.ajax({
             type: "POST",
@@ -94,10 +98,10 @@ function matchInit() {
             console.log(textStatus);
         });
     }
-    
+
     function nextCard() {
         matches.shift();
-        
+
         //There are two cards, one being visible, the other one
         //being the buffer card that has everything loaded into it
         //before the cards being switched
@@ -105,21 +109,21 @@ function matchInit() {
             $("#card-2-name").html(matches[0].name);
             $("#card-2-age").html(matches[0].age);
             $("#card-2-image").css("background-image", "url('" + matches[0].photo + "')");
-            
+
             $("#card-2-image").click(function() {
                 globals.match.userSelected = matches[0].fb_id;
                 pageTransition("profile.html", profileInit);
             });
-            
+
             if (matches[0].photo === null) {
                 $("#card-1-image").css("background-color", "black");
             }
-            
+
             $("#match-card-1").animate({
                     opacity: 0.0
-                }, 
-                200, 
-                "linear", 
+                },
+                200,
+                "linear",
                 function() {
                     $("#match-card-1").hide();
                     $("#match-card-2").show();
@@ -127,8 +131,8 @@ function matchInit() {
                     $("#match-card-2").animate({
                             opacity: 1.0
                         },
-                        200, 
-                        "linear", 
+                        200,
+                        "linear",
                         function() {
 
                         }
@@ -136,43 +140,43 @@ function matchInit() {
                 }
             );
         }
-        
+
         else {
             $("#card-1-name").html(matches[0].name);
             $("#card-1-age").html(matches[0].age);
             $("#card-1-image").css("background-image", "url('" + matches[0].photo + "')");
-            
+
             $("#card-1-image").click(function() {
                 globals.match.userSelected = matches[0].fb_id;
                 pageTransition("profile.html", profileInit);
             });
-            
+
             if (matches[0].photo === null) {
                 $("#card-1-image").css("background-color", "black");
             }
-            
+
             $("#match-card-2").animate({
                     opacity: 0.0
-                }, 
-                200, 
-                "linear", 
+                },
+                200,
+                "linear",
                 function() {
                     $("#match-card-2").hide();
                     $("#match-card-1").show();
 
                     $("#match-card-1").animate({
                             opacity: 1.0
-                        }, 
-                        200, 
+                        },
+                        200,
                         "linear",
                         function() {
-                            
+
                         }
                     );
                 }
             );
         }
-        
+
         if (matches.length <= matchCacheSize) {
             updateMatches();
         }
---
+++ www/js/settings.js
@@ -0,0 +1,8 @@
+function settingsInit() {
+    currentPage = "SETTINGS";
+
+    //The back button is clicked
+    $("#settings-messages-side").click(function() {
+        pageTransition("match.html", matchInit);
+    });
+}
