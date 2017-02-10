fcc9f99319ba5be6ce3f9560857288a344d04964
Jalfor <jalerous@gmail.com>
2017-02-10 16:51:23 +1100

    Stopped people submitting stuff in signup unless they've filled everything in

--- www/js/signup/page3.js
+++ www/js/signup/page3.js
@@ -3,30 +3,32 @@ function signupPage3Init() {
     $("#signup-p3-code-input").val(globals.verificationToken);
     
     $("#signup-p3-next").click(function() {
-        $.ajax({
-            type: "POST",
-            dataType: "json",
-            data: {
-                ato: globals.accessToken,
-                req_type: "verifyZid2", 
-                verificationToken: $("#signup-p3-code-input").val()
-            },
-            url: SERVER_ADDRESS + '/user_details.php',
-            success: function(data) {
-                console.log(data);
-                
-                if (data[0] == 0) {    //Correct code
-                    globals.userUniversity = data[1];
-                    pageTransition("signup/page4.html", signupPage4Init);
-                }
-                
-                else {
-                    //Tell them they got it wrong
-                }
-            },
-        }).fail(function(dunnoWhatThisArgumentDoes, textStatus) {
-            console.log(textStatus)
-        });
+        if ($("#signup-p3-code-input").val() != "") {
+            $.ajax({
+                type: "POST",
+                dataType: "json",
+                data: {
+                    ato: globals.accessToken,
+                    req_type: "verifyZid2", 
+                    verificationToken: $("#signup-p3-code-input").val()
+                },
+                url: SERVER_ADDRESS + '/user_details.php',
+                success: function(data) {
+                    console.log(data);
+
+                    if (data[0] == 0) {    //Correct code
+                        globals.userUniversity = data[1];
+                        pageTransition("signup/page4.html", signupPage4Init);
+                    }
+
+                    else {
+                        //Tell them they got it wrong
+                    }
+                },
+            }).fail(function(dunnoWhatThisArgumentDoes, textStatus) {
+                console.log(textStatus)
+            });
+        }
     });
     
     $("#signup-p3-back").click(function() {
